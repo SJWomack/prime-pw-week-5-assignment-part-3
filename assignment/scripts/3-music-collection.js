8f1a1c4009c93721bb418dee55c86349a9fcc73c
Stephen Womack <swomack111@gmail.com>
2022-04-30 12:16:34 -0500

    created show collection function and tested functionality

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,10 +12,18 @@ function addToCollection(title, artist, yearPublished) {
 }
 
 console.log(addToCollection('Back in black', 'ac/dc', 2001)); //testing return
-addToCollection('Blue Album', 'weezer', 2005);
-addToCollection('self titled', 'blink 182', 2007);
-addToCollection('sufferer and the witness', 'rise against', 2009);
-addToCollection('take of your pants and jacket', 'blink 182', 2003);
-addToCollection('deathrace for love', 'juice wrld', 2015);
+console.log(addToCollection('Blue Album', 'weezer', 2005));
+console.log(addToCollection('self titled', 'blink 182', 2007));
+console.log(addToCollection('sufferer and the witness', 'rise against', 2009));
+console.log(addToCollection('take of your pants and jacket', 'blink 182', 2003));
+console.log(addToCollection('deathrace for love', 'juice wrld', 2015));
 console.log(collection); //expect 6 objects in array
 
+function showCollection(array){
+    console.log(`${array.length} records in the collection`);
+    for (let albums of collection){
+        console.log(`${albums.title} by ${albums.artist} published in ${albums.yearPublished}`);
+    }
+}
+
+showCollection(collection);
